793b96d3f01d7a6eb045a6294d666421e16efdac
Steve Ham <steve.ham@kakaomobility.com>
2020-12-19 22:04:49 +0900

    Initial Commit

---
+++ MyMacOSApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyMacOSApp
+//
+//  Created by steve.ham on 2020/12/19.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyMacOSApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyMacOSApp
+//
+//  Created by steve.ham on 2020/12/19.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
