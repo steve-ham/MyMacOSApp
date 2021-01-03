b10045c43aa73c797492f0c21480b11ab18bc3e9
Steve Ham <steve.ham@kakaomobility.com>
2021-01-03 19:36:29 +0900

    working on drag and drop

--- MyMacOSApp/DragAndDropImage.swift
+++ MyMacOSApp/DragAndDropImage.swift
@@ -7,12 +7,13 @@
 
 import Cocoa
 
-class DragAndDropImage: NSObject, NSPasteboardWriting {
+class DragAndDropImage: NSObject, NSPasteboardWriting, NSPasteboardReading {
     
     var name: String!
     var nsURL: NSURL!
     
     init(name: String, nsURL: NSURL) {
+        super.init()
         self.name = name
         self.nsURL = nsURL
     }
@@ -29,5 +30,26 @@ class DragAndDropImage: NSObject, NSPasteboardWriting {
     
     func pasteboardPropertyList(forType type: NSPasteboard.PasteboardType) -> Any? {
         nsURL.pasteboardPropertyList(forType: type)
+    }
+    
+    // MARK: - NSPasteboardReading
+    
+    static func readableTypes(for pasteboard: NSPasteboard) -> [NSPasteboard.PasteboardType] {
+        [.fileURL]
+    }
+    
+    static func readingOptions(forType type: NSPasteboard.PasteboardType, pasteboard: NSPasteboard) -> NSPasteboard.ReadingOptions {
+        .asString
+    }
+    
+    required init?(pasteboardPropertyList propertyList: Any, ofType type: NSPasteboard.PasteboardType) {
+        super.init()
+        nsURL = NSURL(pasteboardPropertyList: propertyList, ofType: type)
+        name = (try? nsURL.absoluteURL?.resourceValues(forKeys: [.localizedNameKey]).localizedName) ?? ""
+        if let _ = NSImage(contentsOf: nsURL as URL) {
+        } else {
+            return nil
+        }
+//        DragAndDropImage
     }
 }
--- MyMacOSApp/DragAndDropTableVC.swift
+++ MyMacOSApp/DragAndDropTableVC.swift
@@ -11,6 +11,7 @@ class DragAndDropTableVC: NSViewController, NSTableViewDataSource, NSTableViewDe
     
     @IBOutlet private weak var tableView: NSTableView! {
         didSet {
+            tableView.registerForDraggedTypes([.fileURL])
             tableView.setDraggingSourceOperationMask(.every, forLocal: false)
         }
     }
@@ -25,7 +26,7 @@ class DragAndDropTableVC: NSViewController, NSTableViewDataSource, NSTableViewDe
     }
     
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        guard let cell = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else { return nil }
+        guard let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "Cell"), owner: self) as? NSTableCellView else { return nil }
         cell.imageView?.image = NSImage(contentsOf: dragAndDropImages[row].nsURL as URL)
         cell.textField?.stringValue = dragAndDropImages[row].name
         return cell
@@ -33,6 +34,46 @@ class DragAndDropTableVC: NSViewController, NSTableViewDataSource, NSTableViewDe
     
     func tableView(_ tableView: NSTableView, pasteboardWriterForRow row: Int) -> NSPasteboardWriting? {
         dragAndDropImages[row]
+    }
+    
+    func tableView(_ tableView: NSTableView, validateDrop info: NSDraggingInfo, proposedRow row: Int, proposedDropOperation dropOperation: NSTableView.DropOperation) -> NSDragOperation {
+        if dropOperation == .above {
+            if info.draggingSource as? NSObject == tableView {
+                // reorder
+            } else {
+                let options: [NSPasteboard.ReadingOptionKey : Any] = [NSPasteboard.ReadingOptionKey.urlReadingFileURLsOnly: true, NSPasteboard.ReadingOptionKey.urlReadingContentsConformToTypes: NSImage.imageTypes]
+                if info.draggingPasteboard.canReadObject(forClasses: [NSURL.self], options: options) {
+                    info.animatesToDestination = true
+                    return .copy
+                }
+            }
+        }
+        return []
+    }
+    
+    func tableView(_ tableView: NSTableView, updateDraggingItemsForDrag draggingInfo: NSDraggingInfo) {
+        if draggingInfo.draggingSource as? NSObject != tableView {
+            let classes = [DragAndDropImage.self, NSPasteboardItem.self]
+            
+            let cell = (tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "Cell"), owner: self) as? NSTableCellView)!
+            
+            var validCount = 0
+            draggingInfo.enumerateDraggingItems(options: .concurrent, for: tableView, classes: classes, searchOptions: [:]) { draggingItem, index, _ in
+                if draggingItem.item is DragAndDropImage, let dragAndDropImage = draggingItem.item as? DragAndDropImage {
+                    draggingItem.draggingFrame = cell.frame
+                    draggingItem.imageComponentsProvider = {
+                        cell.imageView?.image = NSImage(contentsOf: dragAndDropImage.nsURL as URL)
+                        cell.textField?.stringValue = dragAndDropImage.name
+                        return cell.draggingImageComponents
+                    }
+                    validCount += 1
+                } else {
+                    draggingItem.imageComponentsProvider = nil
+                }
+            }
+            draggingInfo.numberOfValidItemsForDrop = validCount
+            draggingInfo.draggingFormation = .list
+        }
     }
     
     @IBAction private func clickAddButton(_ sender: NSButton) {
